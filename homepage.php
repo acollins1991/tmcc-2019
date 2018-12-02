3817cc19a445f8daed2eecbe54e41e4e0d8f7f62
Anthony Collins <anthony@themarketingcampaigncompany.co.uk>
2018-12-02 10:33:26 +0000

    Jumbotron setup

---
+++ homepage.php
@@ -0,0 +1,34 @@
+<?php /* Template Name: Homepage Template */
+  get_header();
+?>
+
+<div uk-height-viewport="offset-bottom: 20" class="uk-flex uk-flex-column uk-flex-between">
+
+  <div>
+    <div class="uk-container">
+      <nav class="uk-flex uk-flex-between uk-flex-middle uk-padding-small uk-padding-remove-horizontal">
+        <div>
+          <a href="/">
+            <img src="https://via.placeholder.com/150x50">
+          </a>
+        </div>
+        <div>
+          <a class="uk-button uk-button-default" href="#">Contact Us</a
+        </div>
+      </nav>
+    </div>
+  </div>
+
+  <div>
+    <div class="uk-container uk-container-xsmall">
+      <h1 class="uk-text-center">A PR + Communications agency for financial sector clients</h1>
+    </div>
+  </div>
+
+  <div class="">
+    test
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
