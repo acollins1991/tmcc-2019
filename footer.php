65bf5f82ace67783cd31a272bfcc7e84d19cc177
Anthony Collins <anthony@themarketingcampaigncompany.co.uk>
2018-12-02 10:10:07 +0000

    SHould ignore .maps completely

--- footer.php
+++ footer.php
@@ -13,5 +13,20 @@
 
 <?php wp_footer(); ?>
 
+<div class="uk-container uk-margin-top">
+<hr/>
+<div class="uk-flex uk-flex-between uk-flex-middle">
+    <div>
+			<span class="uk-h6">Copyright The Marketing Campaign Company <?php echo date('Y'); ?></span>
+		</div>
+		<div>
+			<ul class="uk-iconnav">
+			    <li><a href="" uk-icon="icon: twitter; ratio: 0.8"></a></li>
+					<li><a href="" uk-icon="icon: linkedin; ratio: 0.8"></a></li>
+			</ul>
+		</div>
+</div>
+</div>
+
 </body>
 </html>
