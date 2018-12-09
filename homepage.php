5ccb471ab4b8445509f0b769af1617690b39d580
Anthony Collins <anthony@themarketingcampaigncompany.co.uk>
2018-12-09 15:14:51 +0000

    homepage structure

--- homepage.php
+++ homepage.php
@@ -2,8 +2,7 @@
   get_header();
 ?>
 
-<div uk-height-viewport="offset-bottom: 20" class="uk-flex uk-flex-column uk-flex-between">
-
+<div uk-height-viewport="offset-bottom: 12" class="uk-flex uk-flex-column uk-flex-between">
   <div>
     <div class="uk-container">
       <nav class="uk-flex uk-flex-between uk-flex-middle uk-padding-small uk-padding-remove-horizontal">
@@ -18,17 +17,118 @@
       </nav>
     </div>
   </div>
-
   <div>
-    <div class="uk-container uk-container-xsmall">
-      <h1 class="uk-text-center">A PR + Communications agency for financial sector clients</h1>
+    <div class="uk-container">
+      <img src="https://via.placeholder.com/150x50">
+      <h1 class="uk-h2">A PR + Communications agency for the insurance and financial sector</h1>
+      <a class="uk-button uk-button-default" href="/contact-us">Connect with us</a>
     </div>
   </div>
-
-  <div class="">
-    test
+  <div>
+    <div class="uk-container">
+      <div>
+        <div class="tmcc-home-hero __link">
+          <span class="uk-h3">Get to know us</span>
+          <p>Scroll down to learn more</p>
+          <span uk-icon="icon: arrow-down"></span>
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 
+<div class="uk-section uk-background-secondary uk-light">
+  <div class="uk-container">
+    <div uk-grid>
+      <div class="uk-width-3-5">
+        <h2>About Us</h2>
+        <p>We are an experienced team with many years' experience across a number of sectors including financ - general insurance, international & domestic health insurance, life & pensions - and healthcare. Our approach to marketing works equally well across many commercial segments and we help clients' create and execute bespoke marketing campaigns tailored to their bueinss needs.</p>
+        <blockquote>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          <footer>Copyright (c) 2018 Copyright Holder All Rights Reserved.</footer>
+        </blockquote>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/300x500">
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="uk-section uk-container">
+  <h2>Our Services</h2>
+  <p>We are an experienced team with many years' experience across a nuber of sectors. We are an experienced team with many years' experience across a nuber of sectors.</p>
+  <ul class="uk-list uk-list-divider">
+      <li>
+        <h3>Public Relations</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <a href="#">Learn More</a>
+      </li>
+      <li>
+        <h3>Public Relations</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <a href="#">Learn More</a>
+      </li>
+      <li>
+        <h3>Public Relations</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <a href="#">Learn More</a>
+      </li>
+  </ul>
+</div>
+
+<div class="uk-section uk-container uk-container-xsmall">
+  <h2 class="uk-text-center">Who we've worked with</h2>
+  <div uk-grid class="uk-child-width-1-4">
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/150x150">
+    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
